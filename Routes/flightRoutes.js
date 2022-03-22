12ff03f68c602efc51e2c946bfb5f728252f197a
Admin <tamsynsteed@gmail.com>
2022-03-22 11:54:55 +0200

    make it better

--- Routes/flightRoutes.js
+++ Routes/flightRoutes.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const router= express.Router()
 const flight= require('../Models/flightModel')
-const getFlight  = require("../middleware/retriever");
+const {getFlight}  = require("../middleware/retriever");
 //Get all
 router.get('/', async (req, res)=> {
     try{
